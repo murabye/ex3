72481a43b7557921076d804cdf6153822911f2c9
murabye <vlada_kuz2011@mail.ru>
2017-06-02 05:23:28 +0500

    тестик меняются, формулы упрощаются

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -7,13 +7,16 @@ namespace ex3
     {
         private static void Main()
         {
-            var x = Ask.RealNum("Введите координату x: ");
-            var y = Ask.RealNum("Введите координату у: ");
+            while (true)
+            {
+                var x = Ask.RealNum("Введите координату x: ");
+                var y = Ask.RealNum("Введите координату у: ");
+                    
+                var ans = (y<=0 && y>=-2 || Math.Abs(x)>=y && y>=0) && x<=1 && x>=-1;
 
-            var ans = (y<=0 && y>=-2 || x<=y && y>=0 || -x<=y && y >= 0) && x<=1 && x<=1;
-
-            Console.WriteLine("Ответ: "+ans);
-            OC.Stay();
+                Console.WriteLine("Ответ: "+ans);
+                OC.Stay();
+            }
         }
     }
 }
