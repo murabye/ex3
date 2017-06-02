2a0545c70779c85a171c48b9c612db5d1d23a51d
murabye <vlada_kuz2011@mail.ru>
2017-06-02 05:02:46 +0500

    черновая версия

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyLib;
 
 namespace ex3
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var x = Ask.RealNum("Введите координату x: ");
+            var y = Ask.RealNum("Введите координату у: ");
+
+            var ans = (y<=0 && y>=-2 || x<=y && y>=0 || -x<=y && y >= 0) && x<=1 && x<=1;
+
+            Console.WriteLine("Ответ: "+ans);
+            OC.Stay();
         }
     }
 }
